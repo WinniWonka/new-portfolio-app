0c38eda4403feee128c5539983886430e460d3e6
Andreas <andreaslie17@gmail.com>
2024-09-29 13:18:05 +0700

    revision name again

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Joefolio'`, () => {
+  it(`should have as title 'Andreas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Joefolio');
+    expect(app.title).toEqual('Andreas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Joefolio app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Andreas app is running!');
   });
 });
